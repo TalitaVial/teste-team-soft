7b745f968f599beb206d7d6e6ab60d47df9145da
Talita Vial <talitavialsilva1@gmail.com>
2023-04-06 14:53:20 -0400

    refactor: componente de header teste para versão web

--- deliverize/src/components/header.jsx
+++ deliverize/src/components/header.jsx
@@ -1,23 +1,33 @@
 
 import deliverize from '../assets/deliverize.png'
 import arrow_left from '../assets/arrow_left.png'
-import user from '../assets/user.png'
+// import user from '../assets/user.png'
+// import shopping from '../assets/shopping.png'
 import '../styles/header.css'
 
 export default function Header() {
   return (
     <div className='header' >
+   
       <div className='header__logo'>
         <img src={arrow_left} alt='arrow'/>
         <img src={deliverize} alt='Logo'/>
-        <div>
-          <select className='header__selected' name="" id="">
+        </div>
+          {/* <select className='header__selected' name="" id="">
             <option value=""></option>
           </select>
           <input className='header__search' type="search" />
+        <div className='header__login'>
+        <div className='header--user'>
+          <img src={user} alt='user'/>
+          <p className='header--text'>Entrar</p>
         </div>
-        <img src={user} alt='user'/>
-      </div>
+        <div className='header--user'>
+          <img src={shopping} alt='shopping'/>
+          <p className='header--text'>Carrinho</p>
+        </div>
+ 
+        </div> */}
 
     </div>
   );
