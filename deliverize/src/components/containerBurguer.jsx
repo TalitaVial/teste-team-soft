f48b1d3be7cc9b7c27605105062984e5c79d26c5
Talita Vial <talitavialsilva1@gmail.com>
2023-04-07 16:53:13 -0400

    refactor: organizar codigo css, renomear classes

--- deliverize/src/components/containerBurguer.jsx
+++ deliverize/src/components/containerBurguer.jsx
@@ -2,31 +2,41 @@ import Burguer from    '../assets/foto.png'
 import '../styles/containerBurguer.css'
 import ListMenu from './listMenu'
 import ContainerToAdd from './containerToAdd';
+import ButtonToAdd from './buttonToAdd';
+import ContainerChekBox from './containerChekBox';
+
 
 export default function ContainerBurguer(){
     return(
         <div className='container'>
-            <div className='container__Burguer'>
-            <img className='container--imgBurguer' src={Burguer} alt="FotoBurguer" />
-            </div>
-            <div className='container__description'>
-                <div className='container--title'>
-                    <h3>Oferta picanha cheddar bacon</h3>
+            <div className='container__webBurger'>
+                <div className='container__Burger'>
+                    <img className='container--imgBurger' src={Burguer} alt="FotoBurger" />
                 </div>
-                <div className='container__ingredients'>
-                    <p>Delicioso hambúrguer de picanha, molho de picanha,
-                    cebola crispy, bacon, queijo cheddar, molho cheddar e pão mix de gergelim,
-                    acompanhamento e bebida.</p>
-                </div>
-                <div className='container--money'>
-                    <p className='container--money--sale'>R$31,99</p>
-                    <p className='container--money--noSale'>R$34,95</p>
-                </div>    
-                </div>
-                <div className='container__menu'>
-                    <ContainerToAdd/>
-                    <ListMenu/>
+                <div className='container__description'>
+                    <div className='container--title'>
+                        <h3>Oferta picanha cheddar bacon</h3>
+                    </div>
+                    <div className='container__ingredients'>
+                        <p>Delicioso hambúrguer de picanha, molho de picanha,
+                        ebola crispy, bacon, queijo cheddar, molho cheddar e pão mix de gergelim,
+                        acompanhamento e bebida.</p>
+                    </div>
+                    <div className='container--money'>
+                        <p className='container--money--sale'>R$31,99</p>
+                        <p className='container--money--noSale'>R$34,95</p>
+                    </div>  
                 </div>  
+            </div>
+            <div className='container__menu'>
+            <div className='container__menu--itens'>
+                <ContainerToAdd titulo="Adicionar Ingredientes" descricao="Até 8 ingredientes."/>
+                <ListMenu/>
+                <ContainerToAdd titulo="Precisa de Talher?"/>
+                <ContainerChekBox/>
+                <ButtonToAdd/>              
+            </div>
+            </div>  
         </div>
     )
 }
