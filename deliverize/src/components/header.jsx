61e42cdbadc9ea551e4c3468b95501675c0e12b0
Talita Vial <talitavialsilva1@gmail.com>
2023-04-08 11:05:49 -0400

    feat: header responsivo web

--- deliverize/src/components/header.jsx
+++ deliverize/src/components/header.jsx
@@ -1,34 +1,37 @@
 
-import deliverize from '../assets/deliverize.png'
+
 import arrow_left from '../assets/arrow_left.png'
-// import user from '../assets/user.png'
-// import shopping from '../assets/shopping.png'
+import Deliverize from '../assets/Deliverize.svg'
+import user from '../assets/user.svg'
+import shopping from '../assets/shopping.svg'
 import '../styles/header.css'
 
 export default function Header() {
   return (
     <div className='header' >
-   
       <div className='header__logo'>
-        <img src={arrow_left} alt='arrow'/>
-        <img src={deliverize} alt='Logo'/>
-        </div>
-          {/* <select className='header__selected' name="" id="">
+        <img className='header--arrow' src={arrow_left} alt='arrow'/>
+        <img className='header--deliverize' src={Deliverize} alt='Logo'/>
+      </div>
+      <div className='header--web'>
+        <div  className='header__selected'>
+          <p className='header--placeholder'>Entrega:</p> 
+          <select name="selected" id="selected">
             <option value=""></option>
           </select>
-          <input className='header__search' type="search" />
+        </div>
+        <input className='header__search' type="search" name='search' id='search' placeholder='Busque por estabelecimento ou produtos' />
         <div className='header__login'>
-        <div className='header--user'>
-          <img src={user} alt='user'/>
-          <p className='header--text'>Entrar</p>
+          <div className='header--user'>
+            <img className='header--user--img' src={user} alt='user'/>
+            <p className='header--text'>Entrar</p>
+          </div>
+          <div className='header--user'>
+            <img src={shopping} alt='shopping'/>
+            <p className='header--text'>Carrinho</p>
+          </div>
         </div>
-        <div className='header--user'>
-          <img src={shopping} alt='shopping'/>
-          <p className='header--text'>Carrinho</p>
-        </div>
- 
-        </div> */}
-
+      </div>
     </div>
   );
 }
