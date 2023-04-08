4568d8971e4cb3dd302dcd72addc91480a2227e1
Talita Vial <talitavialsilva1@gmail.com>
2023-04-08 11:07:21 -0400

    feat: props no componente containerToAdd (talher e ingredientes)

--- deliverize/src/components/containerToAdd.jsx
+++ deliverize/src/components/containerToAdd.jsx
@@ -1,10 +1,10 @@
 import '../styles/containerToAdd.css'
 
-export default function ContainerToAdd(){
+export default function ContainerToAdd({titulo, descricao}){
     return(
         <div className="container__ToAdd">
-            <p className='container__text'>Adicionar Ingredientes</p>
-            <p className='container__text--limit'>Até 8 ingredientes.</p>
+            <p className='container__text'>{titulo}</p>
+            <p className='container__text--limit'>{descricao}</p>
         </div>
     )
 }
--- deliverize/src/components/listMenu.jsx
+++ deliverize/src/components/listMenu.jsx
@@ -8,10 +8,9 @@ export default function ListMenu(){
                 <div className='card__item'>
                     <p className='card--title'>Queijo cheddar</p>
                     <p className='card--money'>+ R$4,99</p>
+                </div>
+                <ButtonMenu/>
             </div>
-            <ButtonMenu/>
-            
-        </div>
             <div className='card__line'></div>
         </div>
         
