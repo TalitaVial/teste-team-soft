34745a6a8402ea8791c399ecd185d8c0b446e6bd
Talita Vial <talitavialsilva1@gmail.com>
2024-06-03 20:55:17 -0400

    test: testes no header componente

--- src/components/tests/header.spec.tsx
+++ src/components/tests/header.spec.tsx
@@ -4,14 +4,31 @@ import '@testing-library/jest-dom/extend-expect';
 import Header from '../header/header';
 import { ContextOrders } from "../../provider/contextOrders";
 
-describe("Header", () => {
-  it("should render correctly additional header application", () => {
+const renderComponente = () =>{
     const mockOrders = { orders: [] }; 
     render(
-      <ContextOrders.Provider value={mockOrders}>
-        <Header />
-      </ContextOrders.Provider>
-    );
+        <ContextOrders.Provider value={mockOrders}>
+          <Header />
+        </ContextOrders.Provider>
+      );
+}
+
+describe("Header", () => {
+  it("should render correctly additional header tittle application", () => {
+    renderComponente();
     expect(screen.getByText('Entrega:')).toBeInTheDocument();
   });
+
+  it("should render correctly additional header IMG header--arrow application",()=>{
+    renderComponente();
+    const arrowImage = screen.getByAltText('arrow');
+    expect(arrowImage).toBeInTheDocument();
+  });
+
+  it("should render correctly additional header IMG header--deliverize application",()=>{
+    renderComponente();
+    const logoImage = screen.getByAltText('Logo');
+    expect(logoImage).toBeInTheDocument();
+  })
 });
+
